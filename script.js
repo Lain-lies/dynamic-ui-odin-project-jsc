c71fb51f827944d2ff6fec53b8e719991c4ffd3e
Kenjie Tarasona-wsl <148972543+voidKing00@users.noreply.github.com>
2025-03-11 18:36:49 +0800

    Implement image carousel

--- script.js
+++ script.js
@@ -17,3 +17,52 @@ dropDownButton.addEventListener("click", () => {
         expanded = false;
     }
 });
+
+
+const parent = document.querySelector("#screen");
+const images = [...document.querySelectorAll(".image-container")];
+const left = document.querySelector(".carousel button:first-child");
+const right = document.querySelector(".carousel button:last-child");
+const max = images.length - 1;
+
+//default
+let currentlyShown = 0;
+
+for(let i = 1; i < images.length; i++){
+    
+    parent.removeChild(images[i]);
+
+}
+
+console.log(parent);
+left.addEventListener("click", () => {
+    
+    parent.removeChild(images[currentlyShown]);
+
+    if(currentlyShown === 0){
+        
+        currentlyShown = max;
+
+    }else{
+
+        currentlyShown--;
+    }
+
+    parent.appendChild(images[currentlyShown]);
+});
+
+right.addEventListener("click", () => {
+    
+    parent.removeChild(images[currentlyShown]);
+
+    if(currentlyShown === max){
+        
+        currentlyShown = 0;
+
+    }else{
+
+        currentlyShown++;
+    }
+
+    parent.appendChild(images[currentlyShown]);
+});
